a8830f9da7c884fb5b1378a252802ace4333571d
hussein16hamada <boobroid1661996>
2020-01-18 16:15:13 +0200

    Delete the dummy Data  and use Glide as an Example

--- app/src/main/java/com/hussein/dagger/AuthActivity.java
+++ app/src/main/java/com/hussein/dagger/AuthActivity.java
@@ -2,9 +2,13 @@ package com.hussein.dagger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
 import com.hussein.dagger.di.DaggerAppComponent;
 
 import javax.inject.Inject;
@@ -12,16 +16,24 @@ import javax.inject.Inject;
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class AuthActivity extends DaggerAppCompatActivity {
+
     @Inject
-    String text;
+    RequestManager requestManager;
+
+    @Inject
+    Drawable logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        setLogo();
 
+    }
 
+    private void setLogo(){
+        requestManager.load(logo)
+                .into((ImageView) findViewById(R.id.login_logo));
     }
 }
--- app/src/main/java/com/hussein/dagger/di/AppModule.java
+++ app/src/main/java/com/hussein/dagger/di/AppModule.java
@@ -1,5 +1,15 @@
 package com.hussein.dagger.di;
 
+import android.app.Application;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.hussein.dagger.R;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -10,8 +20,25 @@ public class AppModule {
     // put other dependencies that are not related to activities
     // like Glide , Retrofit ....
     // Methods here better to be static like doc said
+
     @Provides
-    static String Somthing(){
-        return "testttttttt";
+    static RequestOptions provideRequestOptions(){
+
+        return RequestOptions.placeholderOf(R.drawable.white_background)
+                .error(R.drawable.white_background);
+    }
+
+    @Provides
+    static RequestManager provideGlideInstance(Application application,RequestOptions requestOptions){
+
+        return Glide.with(application)
+                .setDefaultRequestOptions(requestOptions);
+    }
+
+
+    @Provides
+   static Drawable provideAppDrawable(Application application){
+
+        return ContextCompat.getDrawable(application,R.drawable.logo);
     }
 }
